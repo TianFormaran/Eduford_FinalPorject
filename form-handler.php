294051feeb4361e93aa6a22e38f899ccf9f977dd
Rafael Formaran <113695168+TianFormaran@users.noreply.github.com>
2025-05-27 19:05:55 +0400

    Updated domain from .com to .site

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@edufordeducation.com';
+$email_from = 'info@edufordeducation.site';
 
 $email_subject = 'New Form Submission';
 
